9dc70021b8ec1bdcaede860cab885bc02e92dafd
Niklas Sauer <nik.sauer@me.com>
2018-06-13 16:00:02 +0200

    Initial Commit

---
+++ ToolKit_iOS/ToolKit_iOS.h
@@ -0,0 +1,19 @@
+//
+//  ToolKit_iOS.h
+//  ToolKit_iOS
+//
+//  Created by Niklas Sauer on 13.06.18.
+//  Copyright © 2018 SauerStudios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ToolKit_iOS.
+FOUNDATION_EXPORT double ToolKit_iOSVersionNumber;
+
+//! Project version string for ToolKit_iOS.
+FOUNDATION_EXPORT const unsigned char ToolKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToolKit_iOS/PublicHeader.h>
+
+
